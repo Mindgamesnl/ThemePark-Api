c901853f840d2f90ee2ba1ce7aec8354a9aa2e8e
Mindgamesnl <matsmoolhuizen@gmail.com>
2019-08-30 20:46:50 +0200

    No need for a big switch

--- src/main/java/me/paradoxpixel/themepark/api/attraction/component/Status.java
+++ src/main/java/me/paradoxpixel/themepark/api/attraction/component/Status.java
@@ -8,28 +8,7 @@ public enum Status {
     INACTIVE, GLOBAL;
 
     public static Status getStatus(String string) {
-        if(string == null)
-            return null;
-
-        string = string.toUpperCase();
-        switch(string) {
-            case "CONSTRUCTION":
-                return Status.CONSTRUCTION;
-            case "OPEN":
-                return Status.OPEN;
-            case "CLOSED":
-                return Status.CLOSED;
-            case "MAINTENANCE":
-                return Status.MAINTENANCE;
-            case "MALFUNCTION":
-                return Status.MALFUNCTION;
-            case "ACTIVE":
-                return Status.ACTIVE;
-            case "INACTIVE":
-                return Status.INACTIVE;
-            default:
-                return null;
-        }
+        return valueOf(string);
     }
 
 }
